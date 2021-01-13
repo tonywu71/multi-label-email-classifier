f359f0cd71235b36464345d9d63801eb7d821567
Matthias Lesage <matthias.lesage@student-cs.fr>
2021-01-13 09:05:09 +0100

    Small update to take into account the time in the json

--- ml_groundtruth/ml_matthias.py
+++ ml_groundtruth/ml_matthias.py
@@ -221,16 +221,13 @@ def main(n_iter=1000, n_jobs=8):
 
     random_search.fit(X_full, y)
     end = time.time()
-    print('|', str(end-start), 'elapsed.')
-    print('| Meilleur paramètre :')
-    print(random_search.best_params_)
 
     print(f"Random Search results: {random_search.cv_results_}\n\n")
     print(f"Random Search best params: {random_search.best_params_}\n\n")
 
     # Saving results in files
     with open('random_search_results.json', "w") as f:
-            json.dump({"results": random_search.cv_results_, "best_params": random_search.best_params_}, f)
+            json.dump({"results": random_search.cv_results_, "best_params": random_search.best_params_, "time":str(end-start)}, f)
 
 if __name__=='__main__':
     main()
